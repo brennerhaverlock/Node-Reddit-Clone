9aa164e2e1a7d89c8d69fca57a3f4ec21f1fa629
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-05-06 16:07:55 -0700

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,63 +1,75 @@
 const Post = require('../models/post.js');
+const User = require('../models/user.js');
 
 module.exports = app => {
+  //Index
   app.get('/', (req, res) => {
     var currentUser = req.user;
-
-    Post.find({}).lean()
-        .then (posts => {
-            res.render("posts-index", { posts, currentUser });
-        })
-        .catch(err => {
-            console.log(err.message);
-        });
-    });
+    // res.render('home', {});
+    console.log(req.cookies);
+    Post.find({}).lean().populate('author')
+    .then(posts => {
+        res.render('posts-index', { posts, currentUser });
+        // res.render('home', {});
+    }).catch(err => {
+        console.log(err.message);
+    })
+})
 
 app.get('/posts/new', (req, res) => {
     // var currentUser = req.user;
     res.render('posts-new');
         });
 // CREATE
-// CREATE
-  // CREATE
 app.post("/posts/new", (req, res) => {
   if (req.user) {
-    var post = new Post(req.body);
-
-    post.save(function(err, post) {
-      return res.redirect(`/`);
-    });
+      var post = new Post(req.body);
+        post.author = req.user._id;
+      post
+          .save()
+          .then(post => {
+              return User.findById(req.user._id);
+          })
+          .then(user => {
+              user.posts.unshift(post);
+              user.save();
+              // REDIRECT TO THE NEW POST
+              res.redirect(`/posts/${post._id}`);
+          })
+          .catch(err => {
+              console.log(err.message);
+          });
   } else {
-    return res.status(401); // UNAUTHORIZED
+      return res.status(401); // UNAUTHORIZED
   }
 });
 
 
   // SHOW
   app.get("/posts/:id", function (req, res) {
-      var currentUser = req.user;
-      Post.findById(req.params.id).populate('comments').lean()
-          .then(post => {
-              res.render("posts-show", { post, currentUser });
-          })
-          .catch(err => {
-              console.log(err.message);
-          });
-  });
-
-// });
-// SUBREDDIT
-// SUBREDDIT
-app.get("/n/:subreddit", function (req, res) {
     var currentUser = req.user;
-    Post.find({ subreddit: req.params.subreddit }).lean()
-        .then(posts => {
-            res.render("posts-index", { posts, currentUser });
+    // LOOK UP THE POST
+
+    Post.findById(req.params.id).lean().populate('comments').populate('author')
+        .then(post => {
+            res.render("posts-show", { post, currentUser });  
         })
         .catch(err => {
-            console.log(err);
+            console.log(err.message);
         });
-    });
+});
+
+// SUBREDDIT
+app.get("/n/:subreddit", function (req, res) {
+  var currentUser = req.user;
+  Post.find({ subreddit: req.params.subreddit }).lean().populate('author')
+      .then(posts => {
+          res.render("posts-index", { posts, currentUser });
+      })
+      .catch(err => {
+          console.log(err);
+      });
+});
 
 
 
--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  content: { type: String, required: true }
+  content: { type: String, required: true },
+  author : { type: Schema.Types.ObjectId, ref: "User"}
+
 },
   {timestamps: {createdAt: 'created_at'}}
 );
